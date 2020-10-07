4c35ba712d20af99eafd0bef76e5a1e5afca883b
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-07 17:52:31 -0300

    feat: add function to return avatar with a url for visualization

--- backend/src/modules/users/infra/typeorm/entities/User.ts
+++ backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -5,8 +5,9 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
 
-import { Exclude } from 'class-transformer';
+import uploadConfig from '@config/upload';
 
 @Entity('users')
 class User {
@@ -31,6 +32,19 @@ class User {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    if (!this.avatar) return null;
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+
+      default:
+        return null;
+    }
+  }
 }
 
 export default User;
