ef70a1d084abe52dfe8cb2f62e37a1cdc5ddcca6
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-07 16:34:14 -0300

    feat: profile routing

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
+router.use('/profile', profileRouter);
 
 export default router;
