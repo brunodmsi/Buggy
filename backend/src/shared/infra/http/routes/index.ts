a42dbf7a7f5b771ae115bf6e86614aec52891baa
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-07 19:52:09 -0300

    feat: forgot password funcionality

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
 router.use('/profile', profileRouter);
+router.use('/password', passwordRouter);
 
 export default router;
