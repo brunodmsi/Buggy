2d8f0c4ccd0a48345cb890e2e6e0faaba32e0ebe
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-07 16:11:02 -0300

    feat: user secure routing
    Creation of middleware that ensures that the user needs to have a JWT token to be able to access some of application routes.

---
+++ backend/src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth';
+import { verify } from 'jsonwebtoken';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { secret } = authConfig.jwt;
+    const decoded = verify(token, secret);
+
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
