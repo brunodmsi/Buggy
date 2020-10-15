60f9e4fd09932eccc3f8eaba77cba12dce562ecc
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-14 23:34:39 -0300

    fix: add ensureAuthenticated middleware to projects routes

--- backend/src/modules/projects/infra/http/routes/projects.routes.ts
+++ backend/src/modules/projects/infra/http/routes/projects.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProjectsController from '../controllers/ProjectsController';
 
 const projectsController = new ProjectsController();
 
 const projectsRouter = Router();
 
+projectsRouter.use(ensureAuthenticated);
 projectsRouter.post('/', projectsController.create);
 
 export default projectsRouter;
