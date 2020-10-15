e39bad8d3cfbd62465c1316fdd5b973faec2f884
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-15 00:29:52 -0300

    feat: user-projects dependendy injection

--- backend/src/modules/projects/providers/index.ts
+++ backend/src/modules/projects/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IProjectsRepository from '../repositories/IProjectsRepository';
 import ProjectsRepository from '../infra/typeorm/repositories/ProjectsRepository';
 
+import IUserProjectsRepository from '../repositories/IUserProjectsRepository';
+import UserProjectsRepository from '../infra/typeorm/repositories/UserProjectsRepository';
+
 container.registerSingleton<IProjectsRepository>(
   'ProjectsRepository',
   ProjectsRepository,
+);
+
+container.registerSingleton<IUserProjectsRepository>(
+  'UserProjectsRepository',
+  UserProjectsRepository,
 );
