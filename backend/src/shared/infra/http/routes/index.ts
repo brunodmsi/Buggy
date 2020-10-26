76bb69a1512310c0b272444fbf58739690ef5080
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-26 17:07:31 -0300

    fix: pattern in projects routing

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -5,11 +5,13 @@ import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
-import projectsRouter from '@modules/projects/infra/http/routes/projects.routes';
+import projectsRouter from '@modules/projects/infra/http/routes';
+import bugsRouter from '@modules/bugs/infra/http/routes/bugs.routes';
 
 const router = Router();
 
 router.use('/projects', projectsRouter);
+router.use('/bugs', bugsRouter);
 
 router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
