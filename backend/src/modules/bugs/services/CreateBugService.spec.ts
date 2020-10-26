f450a2d1aa7210d68e462fd1502508ace0dfdc89
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-26 12:40:28 -0300

    feat: create bug service

---
+++ backend/src/modules/bugs/services/CreateBugService.spec.ts
@@ -0,0 +1,56 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeProjectsRepository from '@modules/projects/repositories/fakes/FakeProjectsRepository';
+import FakeBugsRepository from '../repositories/fakes/FakeBugsRepository';
+import CreateBugService from './CreateBugService';
+
+let fakeBugsRepository: FakeBugsRepository;
+let fakeProjectsRepository: FakeProjectsRepository;
+let createBug: CreateBugService;
+
+describe('CreateProject', () => {
+  beforeEach(() => {
+    fakeBugsRepository = new FakeBugsRepository();
+    fakeProjectsRepository = new FakeProjectsRepository();
+
+    createBug = new CreateBugService(
+      fakeBugsRepository,
+      fakeProjectsRepository,
+    );
+  });
+
+  it('should be able to create a new project', async () => {
+    const project = await fakeProjectsRepository.create({
+      name: 'Scient',
+      description: 'A new way of opening a bank account',
+      url: 'https://scient.demasi.dev',
+      owner_id: 'non-existing-user-id',
+    });
+
+    const bug = await createBug.execute({
+      title: 'Erro na responsividade',
+      description: 'Div está dando overflow na rota X',
+      type: 'WEB',
+      project_id: project.id,
+      group: 0,
+      status: 0,
+      date_limit: new Date(2020, 8, 20, 11, 0),
+    });
+
+    expect(bug).toHaveProperty('id');
+  });
+
+  it('should not be able to create a bug with non-existing project', async () => {
+    await expect(
+      createBug.execute({
+        title: 'Erro na responsividade',
+        description: 'Div está dando overflow na rota X',
+        type: 'WEB',
+        project_id: 'non-existing-project-id',
+        group: 0,
+        status: 0,
+        date_limit: new Date(2020, 8, 20, 11, 0),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ backend/src/modules/bugs/services/CreateBugService.ts
@@ -0,0 +1,57 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IProjectsRepository from '@modules/projects/repositories/IProjectsRepository';
+import IBugsRepository from '../repositories/IBugsRepository';
+
+import Bug from '../infra/typeorm/entities/Bug';
+
+interface IRequest {
+  title: string;
+  description: string;
+  type: string;
+  group: number;
+  status: number;
+  date_limit: Date;
+  project_id: string;
+}
+
+@injectable()
+class CreateBugService {
+  constructor(
+    @inject('BugsRepository')
+    private bugsRepository: IBugsRepository,
+    @inject('ProjectsRepository')
+    private projectsRepository: IProjectsRepository,
+  ) {}
+
+  public async execute({
+    title,
+    description,
+    type,
+    date_limit,
+    group,
+    status,
+    project_id,
+  }: IRequest): Promise<Bug> {
+    const project = await this.projectsRepository.findById(project_id);
+
+    if (!project) {
+      throw new AppError('Project not found');
+    }
+
+    const bug = await this.bugsRepository.create({
+      title,
+      description,
+      type,
+      group,
+      status,
+      date_limit,
+      project_id,
+    });
+
+    return bug;
+  }
+}
+
+export default CreateBugService;
