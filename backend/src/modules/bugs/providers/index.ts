c2fca6249450fac742225f1d355c15994bee7df0
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-27 15:55:59 -0300

    feat: add bug file to dependency injection

--- backend/src/modules/bugs/providers/index.ts
+++ backend/src/modules/bugs/providers/index.ts
@@ -6,9 +6,17 @@ import BugsRepository from '../infra/typeorm/repositories/BugsRepository';
 import IBugDevelopersRepository from '../repositories/IBugDevelopersRepository';
 import BugDevelopersRepository from '../infra/typeorm/repositories/BugDevelopersRepository';
 
+import IBugFilesRepository from '../repositories/IBugFilesRepository';
+import BugFilesRepository from '../infra/typeorm/repositories/BugFilesRepository';
+
 container.registerSingleton<IBugsRepository>('BugsRepository', BugsRepository);
 
 container.registerSingleton<IBugDevelopersRepository>(
   'BugDevelopersRepository',
   BugDevelopersRepository,
+);
+
+container.registerSingleton<IBugFilesRepository>(
+  'BugFilesRepository',
+  BugFilesRepository,
 );
