53b46b679b703878f49b721852b8ee4e984a00f0
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-27 23:13:12 -0300

    fix: missing route

--- backend/src/modules/projects/infra/http/routes/userProjects.routes.ts
+++ backend/src/modules/projects/infra/http/routes/userProjects.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import UsersInProjectController from '../controllers/UsersInProjectController';
+import UserProjectController from '../controllers/UserProjectController';
 import UserProjectsController from '../controllers/UserProjectsController';
 
 const usersInProjectController = new UsersInProjectController();
 const userProjectsController = new UserProjectsController();
+const userProjectController = new UserProjectController();
 
 const userProjectsRouter = Router();
 
@@ -13,5 +15,7 @@ userProjectsRouter.use(ensureAuthenticated);
 
 userProjectsRouter.get('/:project_id/users', usersInProjectController.index);
 userProjectsRouter.get('/users/:user_id', userProjectsController.index);
+
+userProjectsRouter.post('/:project_id/users', userProjectController.create);
 
 export default userProjectsRouter;
