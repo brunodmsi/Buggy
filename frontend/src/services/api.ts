64cd390dc7bc9dff8058a1aacf93d83a83c6667a
Bruno De Masi <brunodemasi1@gmail.com>
2020-10-29 02:08:26 -0300

    fix: changed api baseURL from raw to .env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('@Buggy:token');
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     Authorization: `Bearer ${token}`,
   },
