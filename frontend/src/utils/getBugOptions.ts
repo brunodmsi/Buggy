9d5bdd2726f63007ed5bf0137456807452a9c265
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-02 17:36:55 -0300

    feat: add getBugOptions to have a bug options pattern

---
+++ frontend/src/utils/getBugOptions.ts
@@ -0,0 +1,62 @@
+import { OptionTypeBase, GroupedOptionsType, OptionsType } from 'react-select';
+
+export interface IOption {
+  label: string;
+  value: string;
+  backColor: string;
+}
+
+export const typeOptions: Array<IOption> = [
+  {
+    label: 'WEB',
+    value: 'web',
+    backColor: '#B080F8',
+  },
+  {
+    label: 'BACKEND',
+    value: 'backend',
+    backColor: '#34495e',
+  },
+  {
+    label: 'DESIGN',
+    value: 'design',
+    backColor: '#00a5e3',
+  },
+  {
+    label: 'OUTRO',
+    value: 'outro',
+    backColor: '#74737a',
+  },
+];
+
+export const priorityOptions: Array<IOption> = [
+  {
+    label: 'ALTA',
+    value: 'alta',
+    backColor: '#FF3333',
+  },
+  {
+    label: 'MÉDIA',
+    value: 'media',
+    backColor: '#F1C73F',
+  },
+  {
+    label: 'BAIXA',
+    value: 'baixa',
+    backColor: '#5CD439',
+  },
+];
+
+export const allPriorityWithoutColors:
+  | GroupedOptionsType<OptionTypeBase>
+  | OptionsType<OptionTypeBase>
+  | undefined = priorityOptions.map(option => {
+  return { label: option.label, value: option.value };
+});
+
+export const allTypeWithoutColors:
+  | GroupedOptionsType<OptionTypeBase>
+  | OptionsType<OptionTypeBase>
+  | undefined = typeOptions.map(option => {
+  return { label: option.label, value: option.value };
+});
