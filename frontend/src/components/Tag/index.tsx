706871909caa2cdc78591dafd2b4b99299555d1b
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-02 17:34:22 -0300

    fix(Tag): name may be undefined

--- frontend/src/components/Tag/index.tsx
+++ frontend/src/components/Tag/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Container } from './styles';
 
 interface TagProps {
-  name: string;
+  name: string | undefined;
   backgroundColor: string;
 }
 
 const Tag: React.FC<TagProps> = ({ name, backgroundColor }) => (
-  <Container backgroundColor={backgroundColor}>{name.toUpperCase()}</Container>
+  <Container backgroundColor={backgroundColor}>
+    {name ? name?.toUpperCase() : 'ND'}
+  </Container>
 );
 
 export default Tag;
