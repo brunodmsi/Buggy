0d513aa4e2853f4a6ec9ccba179109533981ab53
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-05 08:50:56 -0300

    feat(Description): add dynamic component (text and form for edit)

--- frontend/src/pages/BugReport/Description/index.tsx
+++ frontend/src/pages/BugReport/Description/index.tsx
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import * as Yup from 'yup';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+
+import api from '../../../services/api';
+import { useToast } from '../../../hooks/toast';
+
+import Spinner from '../../../components/Spinner';
+import Textarea from '../../../components/Textarea';
+import Button from '../../../components/Button';
+import getValidationErrors from '../../../utils/getValidationErrors';
 
 import { Container } from './styles';
 
@@ -7,12 +18,76 @@ interface DescriptionProps {
   bugId: string;
 }
 
-const Description: React.FC<DescriptionProps> = ({ bugId, description }) => {
+const Description: React.FC<DescriptionProps> = ({
+  bugId,
+  description: propDescription,
+}) => {
+  const formRef = useRef<FormHandles>(null);
+  const [description, setDescription] = useState(propDescription);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editLoading, setEditLoading] = useState(false);
+
+  const { addToast } = useToast();
+
+  const handleEditSubmit = useCallback(
+    async data => {
+      try {
+        setEditLoading(true);
+
+        const schema = Yup.object().shape({
+          description: Yup.string().required(),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        await api.patch(`/bugs/${bugId}/description`, data);
+
+        addToast({
+          title: 'Edição feita com sucesso!',
+          type: 'success',
+        });
+
+        setDescription(data.description);
+        setEditLoading(false);
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+
+        setEditLoading(false);
+
+        addToast({
+          title: 'Erro ao editar',
+          description: 'Verifique se está tudo correto e tente novamente',
+          type: 'error',
+        });
+      }
+    },
+    [addToast, bugId],
+  );
+
   return (
-    <Container>
+    <Container onClick={() => !isEdit && setIsEdit(true)}>
       <h2>Descrição</h2>
 
-      <p>{description}</p>
+      {isEdit ? (
+        <Form ref={formRef} onSubmit={handleEditSubmit}>
+          <Textarea name="description" defaultValue={description} />
+
+          <div>
+            <Button className="cancel-button" onClick={() => setIsEdit(false)}>
+              Cancelar
+            </Button>
+
+            <Button type="submit" loading={editLoading} spinnerSize={20}>
+              Salvar
+            </Button>
+          </div>
+        </Form>
+      ) : (
+        <p>{description}</p>
+      )}
     </Container>
   );
 };
--- frontend/src/pages/BugReport/Description/styles.ts
+++ frontend/src/pages/BugReport/Description/styles.ts
@@ -3,4 +3,29 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin-bottom: 25px;
   font-size: 18px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    > div {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+
+      button {
+        width: 150px;
+        margin-top: 5px;
+        margin-bottom: 25px;
+
+        &.cancel-button {
+          background-color: #c53030;
+        }
+
+        & + button {
+          margin-left: 5px;
+        }
+      }
+    }
+  }
 `;
