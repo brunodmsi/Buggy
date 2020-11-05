68b473c6b37040b78dab7225bcf8d43b557b9ef6
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-05 01:49:15 -0300

    feat: developers component

---
+++ frontend/src/pages/BugReport/Developers/index.tsx
@@ -0,0 +1,144 @@
+import React, { useState, useCallback } from 'react';
+import { FiEye, FiX, FiTrash } from 'react-icons/fi';
+import Tooltip from 'little-react-tooltip';
+
+import api from '../../../services/api';
+import { useToast } from '../../../hooks/toast';
+
+import Spinner from '../../../components/Spinner';
+
+import { Container, Modal } from './styles';
+
+import avatarPlaceholder from '../../../assets/avatar_placeholder.png';
+
+import { BugDeveloperData } from '..';
+
+interface IRelation {
+  user: BugDeveloperData;
+}
+
+interface DeveloperProps {
+  bugId: string;
+  projectId: string;
+  developers: Array<IRelation>;
+}
+
+const Developers: React.FC<DeveloperProps> = ({
+  bugId,
+  projectId,
+  developers: devRelations,
+}) => {
+  const [developers, setDevelopers] = useState(() => {
+    const devs = devRelations.map(dev => dev.user);
+
+    return devs;
+  });
+  const [listDevelopersModal, setListDevelopersModal] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState('');
+
+  const { addToast } = useToast();
+
+  const handleFileDelete = useCallback(
+    async (id: string) => {
+      try {
+        setDeleteLoading(id);
+
+        await api.delete(`/bugs/files/${id}`);
+
+        const updatedDevelopers = developers.filter(file => file.id !== id);
+
+        setDevelopers(updatedDevelopers);
+
+        addToast({
+          title: 'Desenvolvedor deletado!',
+          type: 'success',
+        });
+      } catch (err) {
+        addToast({
+          title: 'Erro ao deletar!',
+          description:
+            'Ocorreu um erro ao excluir o desenvolvedor, tente novamente mais tarde',
+          type: 'error',
+        });
+      }
+    },
+    [addToast, developers],
+  );
+
+  return (
+    <Container>
+      {developers.length > 0 && (
+        <>
+          <p>Desenvolvedores</p>
+
+          <section>
+            {developers.map(dev => (
+              <>
+                {dev.avatar_url ? (
+                  <img src={dev.avatar_url} alt={dev.name} />
+                ) : (
+                  <img src={avatarPlaceholder} alt={dev.name} />
+                )}
+              </>
+            ))}
+
+            <button type="button" onClick={() => setListDevelopersModal(true)}>
+              <FiEye size={20} />
+            </button>
+          </section>
+        </>
+      )}
+
+      <Modal isOpen={listDevelopersModal}>
+        <button
+          type="button"
+          className="close-button"
+          onClick={() => setListDevelopersModal(false)}
+        >
+          <FiX size={20} color="#fff" />
+        </button>
+
+        <div>
+          <h3>Todos os desenvolvedores</h3>
+
+          <table>
+            {developers.map(dev => (
+              <tr>
+                <td className="td-dev-img">
+                  {dev.avatar_url ? (
+                    <img src={dev.avatar_url} alt={dev.name} />
+                  ) : (
+                    <img src={avatarPlaceholder} alt={dev.name} />
+                  )}
+                </td>
+
+                <td className="td-dev-name">
+                  <p>{dev.name}</p>
+                </td>
+
+                <td className="td-dev-email">
+                  <p>{dev.email}</p>
+                </td>
+
+                <td className="td-delete">
+                  <button
+                    type="button"
+                    onClick={() => handleFileDelete(dev.id)}
+                  >
+                    {deleteLoading === dev.id ? (
+                      <Spinner size={20} />
+                    ) : (
+                      <FiTrash size={15} color="#fff" />
+                    )}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      </Modal>
+    </Container>
+  );
+};
+
+export default Developers;
---
+++ frontend/src/pages/BugReport/Developers/styles.ts
@@ -0,0 +1,122 @@
+import styled from 'styled-components';
+import ModalContainer from '../../../components/Modal';
+
+export const Modal = styled(ModalContainer)`
+  display: flex;
+  max-width: 800px;
+  width: 800px;
+  max-height: 900px;
+  position: relative;
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  .close-button {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 0px;
+    left: 0px;
+    height: 36px;
+    width: 36px;
+    border-radius: 50%;
+    border: 0;
+    background-color: #5f30e2;
+  }
+
+  table {
+    display: inline-block;
+    max-height: 80%;
+    width: 100%;
+    margin-top: 20px;
+    border-spacing: 0;
+    border-style: hidden;
+
+    tr {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin-bottom: 20px;
+
+      .td-dev-img {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        font-size: 14px;
+        margin-right: 5px;
+
+        img {
+          border-radius: 5px;
+          object-fit: cover;
+          height: 80px;
+          width: 80px;
+        }
+      }
+
+      .td-dev-email {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        width: 100%;
+      }
+
+      .td-dev-name {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        width: 100%;
+        margin-left: 10px;
+      }
+
+      .td-delete {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        button {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: 0;
+          background: #c53030;
+          width: 40px;
+          height: 40px;
+        }
+      }
+    }
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  section {
+    display: flex;
+
+    img {
+      object-fit: cover;
+      width: 35px;
+      height: 35px;
+      border-radius: 50%;
+      margin-right: 4px;
+    }
+
+    button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #e5ecf8;
+      border: 0;
+      background-color: #5f30e2;
+      border-radius: 50%;
+      height: 35px;
+      width: 35px;
+
+      svg {
+        color: #fff;
+      }
+    }
+  }
+`;
