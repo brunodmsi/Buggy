b6c27d74575d526d1b9f15a1afb1b3177fe5e5d4
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-19 20:08:28 -0300

    feat: listener reports in providers

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import '@modules/users/providers';
 import '@modules/projects/providers';
 import '@modules/bugs/providers';
+import '@modules/listener_reports/providers';
 
 import './providers/StorageProvider';
 import './providers/MailTemplateProvider';
